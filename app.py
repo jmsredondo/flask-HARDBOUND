5853257b12b11238662282ed22849a0071e29499
Arvince Neil A. Alcaide <alcaidearvince@gmail.com>
2018-08-11 13:01:46 +0800

    Query database and function for add value in database

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
+
+import sqlite3
+conn = sqlite3.connect('database.db')
+print "Open database successfully"
+#conn.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
+print "Table created successfully"
+conn.close()
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return render_template('hello/index.html')
+
+@app.route('/addrec',methods=['POST','GET'])
+def addrec():
+    if request.method=='POST':
+        nm=request.form['nm']
+        addr=request.form['add']
+        city=request.form['city']
+        pin=request.form['pin']
+        con=sqlite3.connect("database.db")
+        cur=con.cursor()
+        cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin))
+
+    return render_template('hello/addrec.html')
+
+@app.route('/list')
+def list():
+    con=sqlite3.connect("database.db")
+    con.row_factory=sqlite3.Row
+    cur=con.cursor()
+    cur.execute("select * from students")
+    rows=cur.fetchall()
+    return render_template("hello/list.html",rows=rows)
+
 @app.route('/test')
 def testlink():
     return render_template('hello/test.html')
