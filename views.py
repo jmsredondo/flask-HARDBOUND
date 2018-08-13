cb1bd677cbb2fb877666beefc5b933d5a9ea46fd
Daiben Angelo Sanchez <daiben_sanchez@trendmicro.com>
2018-08-13 17:37:25 +0800

    Reverting changes

--- views.py
+++ views.py
@@ -3,25 +3,19 @@ from controllers import *
 from flask import jsonify
 from app import app
 
-@app.route('/users/login',methods=['POST','GET'])
+@app.route('/',methods=['POST','GET'])
 def login():
     return render_template("login.html")
 
-@app.route('/dashboard',methods=['POST','GET'])
+@app.route('/dashboard')
 def dashboard():
-    rows = getlogin()
-    print rows
-    if rows != None:
-        return render_template('dashboard.html')
-    else:
-        return render_template("login.html")
+    return render_template('dashboard.html')
 
-
-@app.route('/bookList',methods=['GET'])
+@app.route('/getbook',methods=['GET'])
 def getbooks():
-    booklist = (getbook())
+    booklist = getbook()
     print((booklist))
-    return render_template('bookList.html',book=booklist)
+    return render_template('hello/getbook.html',book=booklist)
 
 @app.route('/test')
 def testlink():
@@ -31,14 +25,17 @@ def testlink():
 def getregister():
     return render_template('register.html')
 
+@app.route('/genre', methods=['GET'])
+def getgenre():
+    genres = get_genres()
+    return render_template('dispCat_all.html',genres=genres)
+
 @app.route('/users', methods=['POST'])
 def register():
     add_user()
     return render_template('register.html')
 
-@app.route('/userList',methods=['GET'])
+@app.route('/users/list',methods=['GET'])
 def userslist():
     rows = getusers()
     return render_template("userList.html",rows=rows)
-
-
