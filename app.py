0ceff23e98b58993abca8f277a299a769496f832
mark <markryanc23@gmail.com>
2018-08-13 17:29:22 +0800

    removed unused code

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 from flask import Flask
-'''
-import sqlite3
-conn = sqlite3.connect('database.db')
-print "Open database successfully"
-#conn.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
-print "Table created successfully"
-conn.close()
-'''
 
 app = Flask(__name__)
 app.debug = True
