b0612728de6e3cd2154d53005acb8dbfa7811469
mark <markryanc23@gmail.com>
2018-08-13 18:20:07 +0800

    update index

--- views.py
+++ views.py
@@ -3,7 +3,7 @@ from controllers import *
 from flask import jsonify
 from app import app
 
-@app.route('/users/login',methods=['POST','GET'])
+@app.route('/',methods=['POST','GET'])
 def login():
     return render_template("login.html")
 
@@ -27,12 +27,12 @@ def getregister():
 
 @app.route('/genre', methods=['GET'])
 def getgenre():
-    genres = get_genres()
+    #genres = get_genres()
     return render_template('dispCat_all.html',genres=genres)
 
 @app.route('/users', methods=['POST'])
 def register():
-    add_user()
+    adduser()
     return render_template('register.html')
 
 @app.route('/users/list',methods=['GET'])
