8aceec237135fa9808a13b6db471b11ad01c79a7
mark <markryanc23@gmail.com>
2018-08-14 11:46:55 +0800

    fixed config

--- config.py
+++ config.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import os
-#basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'smilymarius'
-   # SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    """ SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-=======
 import os
 #basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -17,5 +7,4 @@ class Config(object):
 
 """ SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 
         'sqlite:///' + os.path.join(basedir, 'app.db')
->>>>>>> bd398f8dce2507552bf3edcf5594a0c8efe20388
     SQLALCHEMY_TRACK_MODIFICATIONS = False"""
