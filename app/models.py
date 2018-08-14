2f97cc46dfd4902081b714cad62e68b524184659
mark <markryanc23@gmail.com>
2018-08-14 11:40:03 +0800

    update login function -> by arvince

--- app/models.py
+++ app/models.py
@@ -46,6 +46,11 @@ def add_user():
     cur.execute("insert into users (username, firstname, lastname, email, balance, phonenumber, password) VALUES (?,?,?,?,?,?,?)", (username, firstname, lastname, email, '0', phonenumber, '123456'))
     db.commit()
 
+def get_genres():
+    cur.execute("select * from genres")
+    genres=cur.fetchall()
+    return genres
+
 """
 class User(db.Model):
     user_id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
@@ -57,6 +62,8 @@ class User(db.Model):
     phone = db.Column(db.Integer, nullable=False)
     password = db.Column(db.String(128),nullable =False)
     password_hash = db.Column(db.String(128), nullable=False)
+    user_type = db.Column(ENUM('Admin','User'),nullable =False)
+    
 
     # posts = db.relationship('Post', backref='author', lazy='dynamic')
 
@@ -70,10 +77,27 @@ class User(db.Model):
             'balance': self.balance,
             'phone': self.phone,
             'password: self.password,
-            'password_hash': self.password_hash
+            'password_hash': self.password_hash,
+            'user_type' :self.user_type
+            
         }
 
         response = '<User %s>' % data
+        return repr(response)
+        
+class genre(db.Model):
+    genre_id =  db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    genre = db.Column(db.String(64), unique=True, nullable=False)
+    type = db.Column(db.String(64), nullable=False)
+
+    def __repr__(self):
+        data = {
+            'Genre ID': self.genre_id,
+            'Genre': self.genre,
+            'Type': self.type
+        }
+
+        response = '<Genre %s>' %data
         return repr(response)
         """
 
