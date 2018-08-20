6ede10dd27a153301c4fef3dae9a50660e8198bd
mark <markryanc23@gmail.com>
2018-08-20 15:10:23 +0800

    fixed routes

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, g
+from flask import render_template
 from flask import flash
 from controllers import *
 from flask import redirect,url_for
@@ -9,7 +9,10 @@ from app import app
 @app.route('/')
 @app.route('/index',methods=['POST','GET'])
 def login():
-    if g.user is None:
+    if 'token' in session:
+        print "token: a  "+ session['token']
+        return render_template("dashboard.html")
+    else:
         #print "token: "+ session['token']
         return render_template("login.html")
     else:
@@ -19,6 +22,7 @@ def login():
 
 @app.route('/dashboard', methods=['POST','GET'])
 def dashboard():
+    #current_user = session['token']
     rows = getlogin()
     if rows is not None:
         current_user = session['token']
@@ -28,168 +32,136 @@ def dashboard():
     else:
         return redirect('/')
 
-@app.before_request
-def before_request():
-    g.user = None
-    if 'token' in session:
-        g.user = session['token']
-
 #getbooktemp and getbook methods used for rendering
 #generating json response
 @app.route('/books')
 def getbooktemp():
-    if g.user is None:
-        return render_template("bookList.html")
-    else:
-        return redirect('/')
+    return render_template("bookList.html")
+
+@app.route('/bookbyid')
+def getbookbyidtem():
+    return render_template("getBook.html")
 
 @app.route('/book',methods=['GET'])
 def getbooks():
-    if g.user is None:
-        current_user = session['token']
-        books = getbook()
-       # genres = getgenres()
-        return jsonify(books)
-    else:
-        return redirect('/')
+    current_user = session['token']
+    books = getbook()
+   # genres = getgenres()
+    return jsonify(books)
 
 @app.route('/book/<bid>', methods=['GET'])
 def getabooks(bid):
-    if g.user is None:
-        books = getabook(bid)
-        return render_template('getBook.html', books=books)
-    else:
-        return redirect('/')
+    books = getabook(bid)
+    return jsonify(books)
 
 @app.route('/book', methods=['POST'])
 def addbooks():
-    if g.user is None:
-        addbook()
-        flash('New book successfully added!')
-        books = getbook()
-        genres = getgenres()
-        return render_template('bookList.html',books=books, genres=genres)
-    else:
-        return redirect('/')
+    addbook()
+    flash('New book successfully added!')
+    books = getbook()
+    genres = getgenres()
+    return render_template('bookList.html',books=books, genres=genres)
 
 @app.route('/book/<bid>')
 def deletebooks(bid):
-    if g.user is None:
-        deletebook(bid)
-        flash('Book successfully deleted.')
-        return redirect('/books')
-    else:
-        return redirect('/')
-
-@app.route('/users', methods=['GET'])
-def getregister():
-    if g.user is None:
-        return render_template('register.html')
-    else:
-        return redirect('/')
-
-@app.route('/users/viewlist', methods=['GET'])
-def userslist():
-    if g.user is None:
-        rows = getusers()
-        print rows
-        return jsonify(rows)
-    else:
-        return redirect('/')
+    deletebook(bid)
+    flash('Book successfully deleted.')
+    return redirect('/books')
 
 @app.route('/users/list', methods=['GET'])
 def listuser():
-    if g.user is None:
-        userslist()
-        return render_template('userList.html')
-    else:
-        return redirect('/')
+    return render_template('userList.html')
+
+@app.route('/user')
+def userbyusernametemp():
+    return render_template('getUser.html')
+
+@app.route('/users', methods=['GET'])
+def userslist():
+    rows = getusers()
+    print rows
+    return jsonify(rows)
 
 @app.route('/users/<uid>', methods=['GET'])
 def getausers(uid):
-    if g.user is None:
-        users = getauser(uid)
-        return render_template('getUser.html', users=users)
-    else:
-        return redirect('/')
+    users = getauser(uid)
+    return jsonify(users)
+
+@app.route('/users', methods=['GET'])
+def getregister():
+    return render_template('register.html')
 
 @app.route('/users', methods=['POST'])
 def register():
-    if g.user is None:
-        adduser()
-        flash('New user successfully added!')
-        return render_template('register.html')
-    else:
-        return redirect('/')
+    adduser()
+    flash('New user successfully added!')
+    return render_template('register.html')
+
+#get list of genres
+@app.route('/genres', methods=['GET'])
+def getgenretemp():
+    return render_template('dispCat_all.html')
+
+@app.route('/genrebyid')
+def getgenreidtemp():
+    return render_template('getGenre.html')
 
 @app.route('/genre', methods=['GET'])
 def getgenre():
-    if g.user is None:
-        genres = getgenres()
-        return render_template('dispCat_all.html',genres=genres)
-    else:
-        return redirect('/')
+    genres = getgenres()
+    return jsonify(genres)
 
 @app.route('/genre/<gid>', methods=['GET'])
 def getagenre(gid):
-    if g.user is None:
-        genres = getagenres(gid)
-        return render_template('getGenre.html', genres=genres)
-    else:
-        return redirect('/')
+    genres = getagenres(gid)
+    return jsonify(genres)
 
 @app.route('/genre', methods=['POST'])
 def addgenres():
-    if g.user is None:
-        addgenre()
-        flash('Genre successfully added!')
-        return redirect('/genre')
-    else:
-        return redirect('/')
+    addgenre()
+    flash('Genre successfully added!')
+    return redirect('/genre')
 
 @app.route('/genre/<gid>')
 def deletegenres(gid):
-    if g.user is None:
-        deletegenre(gid)
-        flash('Genre successfully deleted.')
-        return redirect('/genre')
-    else:
-        return redirect('/')
+    deletegenre(gid)
+    flash('Genre successfully deleted.')
+    return redirect('/genre')
 
 @app.route('/genre/addbook/<gid>', methods=['GET'])
 def getbooktogenre(gid):
-    if g.user is None:
-        books = getunassignedbook(gid)
-        return render_template('bookList2.html',books=books, gid=gid)
-    else:
-        return redirect('/')
+    books = getunassignedbook(gid)
+    return render_template('bookList2.html',books=books, gid=gid)
 
 @app.route('/genre/addbook/<gid>', methods=['POST'])
 def addbooktogenre(gid):
-    if g.user is None:
-        addbookgenre(gid)
-        flash('Genre successfully assigned to book!')
-        return redirect('/genre/addbook/' + gid)
-    else:
-        return redirect('/')
+    addbookgenre(gid)
+    flash('Genre successfully assigned to book!')
+    return redirect('/genre/addbook/' + gid)
 
 @app.route('/library', methods=['GET'])
 def getlibrary():
-    if g.user is None:
-        username = session['token']
-        books = getuserbook(username)
-        return render_template('library.html', books=books)
-    else:
-        return redirect('/')
+    username = 'arvincea'
+    books = getuserbook(username)
+    return render_template('library.html', books=books)
 
 @app.route('/library', methods=['POST'])
 def addlibraries():
-    if g.user is None:
-        addlibrary()
-        flash('Added book to library!')
-        return redirect('/library')
-    else:
-        return redirect('/')
+    addlibrary()
+    flash('Added book to library!')
+    return redirect('/library')
+
+@app.route('/rate', methods=['POST'])
+def addratings():
+    addrating()
+    flash('Added rating!')
+    return redirect('/book')
+
+@app.route('/rate/<bid>', methods=['GET'])
+def getratings(bid):
+    ratings = getrating(bid)
+    flash('Added rating!')
+    return render_template('getRating.html', ratings=ratings)
 
 @app.route('/logout')
 def logout():
