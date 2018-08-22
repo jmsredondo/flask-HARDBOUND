a92f04ac1f3a8f9f0dfd0ec1b56fd5b8d77ee03d
Daiben Angelo Sanchez <daiben_sanchez@trendmicro.com>
2018-08-22 15:17:48 +0800

    Changing ports

--- tests.py
+++ tests.py
@@ -1,4 +1,3 @@
-'''
 # test_bucketlist.py
 import unittest
 import os
@@ -24,7 +23,7 @@ class UserTestCase(unittest.TestCase):
     db.init_app(app)
 
     def setUp(self):
-        self.host = 'http://localhost:5000'
+        self.host = 'http://0.0.0.0:9300'
         self.samplegenre1 = {'type': 'Fiction', 'genre': 'Fantasy'}
         self.samplegenre2 = {'type': '', 'genre': ''}
         self.samplegenre3 = {'type': '1', 'genre': '1'}
@@ -56,7 +55,7 @@ class UserTestCase(unittest.TestCase):
     def test_get_genre_status(self):
         res = res = requests.get(self.host + '/genre')
         self.assertEqual(res.status_code, 200)
-
+'''
     def test_get_books_status(self):
         res = res = requests.get(self.host + '/book')
         self.assertEqual(res.status_code, 200)
@@ -236,7 +235,8 @@ class UserTestCase(unittest.TestCase):
         res = res = requests.get(self.host+'/genre')
         self.assertEqual(res.status_code, 200)
         self.assertIn('Action', str(res.text))
-
+'''
+'''
     def setUp(self):
         """Define test variables and initialize app."""
         self.host = 'http://localhost:5000'
@@ -268,9 +268,8 @@ class UserTestCase(unittest.TestCase):
         res = res = requests.get(self.host+'/users/list')
         self.assertEqual(res.status_code, 200)
         self.assertIn('jsmith', str(res.text))
-
+'''
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
-''' 
