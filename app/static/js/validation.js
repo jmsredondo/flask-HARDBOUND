d97c83ca5079651a0603e3509ba4d42676b9fdb6
Arvince Neil A. Alcaide <alcaidearvince@gmail.com>
2018-08-25 16:10:12 +0800

    Validation in client-side

--- app/static/js/validation.js
+++ app/static/js/validation.js
@@ -1,10 +1,159 @@
+//Register
 $( "#userpost" ).validate({
-
+    rules: {
+        registerUserName: {
+            required: true,
+        },
+        registerFirstname:{
+            required: true,
+            field: { accept: "[a-zA-Z]+" }
+        },
+        registerLastname:{
+            required: true,
+            field: { accept: "[a-zA-Z]+" }
+        },
+        registerEmail:{
+            required: true,
+            email: true
+        },
+        registerPhoneNum:{
+            required: true,
+            digits: true
+        },
+    }
 });
-    $('#register').click(function() {
-        if($("#userpost").valid()) {
-            userregister()
-        }else {
-            alert('incomplete information')
+
+
+$('#register').click(function() {
+    if($("#userpost").valid()) {
+        userregister()
+    }else {
+        alert('incomplete information')
+    }
+});
+
+//User Login
+$( "#loginpost" ).validate({
+    rules: {
+        username: {
+            required: true,
+        },
+        password:{
+            required: true,
+        },
+    }
+});
+$('#login').click(function() {
+    if($("#loginpost").valid()) {
+        userregister()
+    }else {
+        alert('incomplete information')
+    }
+});
+
+//Admin Login
+$( "#loginpostadmin" ).validate({
+    rules: {
+        username: {
+            required: true,
+        },
+        password:{
+            required: true,
+        },
+    }
+});
+
+
+$('#login').click(function() {
+    if($("#loginpostadmin").valid()) {
+        userregister()
+    }else {
+        alert('incomplete information')
+    }
+});
+
+//Add book
+$( "#bookpost" ).validate({
+    rules: {
+        title: {
+            required: true,
+        },
+        description:{
+            required: true,
+        },
+        author:{
+            required: true,
+            field: { accept: "[a-zA-Z]+" }
         }
-    });
+    }
+});
+
+
+$('#').click(function() {
+    if($("#bookpost").valid()) {
+        userregister()
+    }else {
+        alert('incomplete information')
+    }
+});
+
+//Add genre
+$( "#genrepost" ).validate({
+    rules: {
+        title: {
+            addCat: true,
+        }
+    }
+});
+
+
+$('#').click(function() {
+    if($("#genrepost").valid()) {
+        userregister()
+    }else {
+        alert('incomplete information')
+    }
+});
+
+//Add library
+$( "#genrepost" ).validate({
+    rules: {
+        title: {
+            addCat: true,
+        }
+    }
+});
+
+
+$('#').click(function() {
+    if($("#genrepost").valid()) {
+        userregister()
+    }else {
+        alert('incomplete information')
+    }
+});
+
+//Add rating
+$( "#bookpost" ).validate({
+    rules: {
+        title: {
+            required: true,
+        },
+        description:{
+            required: true,
+        },
+        author:{
+            required: true,
+            field: { accept: "[a-zA-Z]+" }
+        }
+    }
+});
+
+
+$('#').click(function() {
+    if($("#bookpost").valid()) {
+        userregister()
+    }else {
+        alert('incomplete information')
+    }
+});
