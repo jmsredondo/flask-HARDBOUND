57416936c58b0d2957043ef41bd3733e30e19344
mark <markryanc23@gmail.com>
2018-08-26 15:57:57 +0800

    temporarily comment out validation

--- app/static/js/validation.js
+++ app/static/js/validation.js
@@ -1,27 +1,27 @@
-//Register
-$( "#userpost" ).validate({
-    rules: {
-        registerUserName: {
-            required: true,
-        },
-        registerFirstname:{
-            required: true,
-            field: { accept: "[a-zA-Z]+" }
-        },
-        registerLastname:{
-            required: true,
-            field: { accept: "[a-zA-Z]+" }
-        },
-        registerEmail:{
-            required: true,
-            email: true
-        },
-        registerPhoneNum:{
-            required: true,
-            digits: true
-        },
-    }
-});
+// //Register
+// $( "#userpost" ).validate({
+//     rules: {
+//         registerUserName: {
+//             required: true,
+//         },
+//         registerFirstname:{
+//             required: true,
+//             field: { accept: "[a-zA-Z]+" }
+//         },
+//         registerLastname:{
+//             required: true,
+//             field: { accept: "[a-zA-Z]+" }
+//         },
+//         registerEmail:{
+//             required: true,
+//             email: true
+//         },
+//         registerPhoneNum:{
+//             required: true,
+//             digits: true
+//         },
+//     }
+// });
 
 
 $('#register').click(function() {
@@ -32,128 +32,128 @@ $('#register').click(function() {
     }
 });
 
-//User Login
-$( "#loginpost" ).validate({
-    rules: {
-        username: {
-            required: true,
-        },
-        password:{
-            required: true,
-        },
-    }
-});
-$('#login').click(function() {
-    if($("#loginpost").valid()) {
-        userregister()
-    }else {
-        alert('incomplete information')
-    }
-});
+// //User Login
+// $( "#loginpost" ).validate({
+//     rules: {
+//         username: {
+//             required: true,
+//         },
+//         password:{
+//             required: true,
+//         },
+//     }
+// });
+// $('#login').click(function() {
+//     if($("#loginpost").valid()) {
+//         userregister()
+//     }else {
+//         alert('incomplete information')
+//     }
+// });
 
-//Admin Login
-$( "#loginpostadmin" ).validate({
-    rules: {
-        username: {
-            required: true,
-        },
-        password:{
-            required: true,
-        },
-    }
-});
+// //Admin Login
+// $( "#loginpostadmin" ).validate({
+//     rules: {
+//         username: {
+//             required: true,
+//         },
+//         password:{
+//             required: true,
+//         },
+//     }
+// });
 
 
-$('#login').click(function() {
-    if($("#loginpostadmin").valid()) {
-        userregister()
-    }else {
-        alert('incomplete information')
-    }
-});
+// $('#login').click(function() {
+//     if($("#loginpostadmin").valid()) {
+//         userregister()
+//     }else {
+//         alert('incomplete information')
+//     }
+// });
 
 //Add book
-$( "#bookpost" ).validate({
-    rules: {
-        title: {
-            required: true,
-        },
-        description:{
-            required: true,
-        },
-        author:{
-            required: true,
-            field: { accept: "[a-zA-Z]+" }
-        }
-    }
-});
+// $( "#bookpost" ).validate({
+//     rules: {
+//         title: {
+//             required: true,
+//         },
+//         description:{
+//             required: true,
+//         },
+//         author:{
+//             required: true,
+//             field: { accept: "[a-zA-Z]+" }
+//         }
+//     }
+// });
+//
+//
+// $('#').click(function() {
+//     if($("#bookpost").valid()) {
+//         userregister()
+//     }else {
+//         alert('incomplete information')
+//     }
+// });
+//
+// //Add genre
+// $( "#genrepost" ).validate({
+//     rules: {
+//         title: {
+//             addCat: true,
+//         }
+//     }
+// });
+//
+//
+// $('#').click(function() {
+//     if($("#genrepost").valid()) {
+//         userregister()
+//     }else {
+//         alert('incomplete information')
+//     }
+// });
+//
+// //Add library
+// $( "#genrepost" ).validate({
+//     rules: {
+//         title: {
+//             addCat: true,
+//         }
+//     }
+// });
 
 
-$('#').click(function() {
-    if($("#bookpost").valid()) {
-        userregister()
-    }else {
-        alert('incomplete information')
-    }
-});
-
-//Add genre
-$( "#genrepost" ).validate({
-    rules: {
-        title: {
-            addCat: true,
-        }
-    }
-});
-
-
-$('#').click(function() {
-    if($("#genrepost").valid()) {
-        userregister()
-    }else {
-        alert('incomplete information')
-    }
-});
-
-//Add library
-$( "#genrepost" ).validate({
-    rules: {
-        title: {
-            addCat: true,
-        }
-    }
-});
-
-
-$('#').click(function() {
-    if($("#genrepost").valid()) {
-        userregister()
-    }else {
-        alert('incomplete information')
-    }
-});
+// $('#').click(function() {
+//     if($("#genrepost").valid()) {
+//         userregister()
+//     }else {
+//         alert('incomplete information')
+//     }
+// });
 
 //Add rating
-$( "#bookpost" ).validate({
-    rules: {
-        title: {
-            required: true,
-        },
-        description:{
-            required: true,
-        },
-        author:{
-            required: true,
-            field: { accept: "[a-zA-Z]+" }
-        }
-    }
-});
+// $("#bookpost" ).validate({
+//     rules: {
+//         title: {
+//             required: true,
+//         },
+//         description:{
+//             required: true,
+//         },
+//         author:{
+//             required: true,
+//             field: { accept: "[a-zA-Z]+" }
+//         }
+//     }
+// });
 
 
-$('#').click(function() {
-    if($("#bookpost").valid()) {
-        userregister()
-    }else {
-        alert('incomplete information')
-    }
-});
+// $('#').click(function() {
+//     if($("#bookpost").valid()) {
+//         userregister()
+//     }else {
+//         alert('incomplete information')
+//     }
+// });
